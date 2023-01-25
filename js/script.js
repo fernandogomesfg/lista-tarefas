c6c1cee28ab3ff8554308168db57149d86cef3da
Fernando Gomes <59961857+fernandogomesfg@users.noreply.github.com>
2023-01-25 20:50:50 +0200

    chore: funcionalidade de marcar tarefa

--- js/script.js
+++ js/script.js
@@ -13,11 +13,11 @@ function addTarefa() {
     // verificar se o input nao esta vazio, nulo ou indefinido
     if ((valorInput !== "") && (valorInput !== null) && (valorInput !== undefined)) {
         let novoItem = `<div id="${cont}" class="item">
-        <div class="item-icone">
-            <i class="iconoir-circle"></i>
+        <div onclick="marcarTarefa(${cont})" class="item-icone">
+            <i id="icone_${cont}" class="iconoir-circle"></i>
         </div>
 
-        <div class="item-nome">
+        <div onclick="marcarTarefa(${cont})" class="item-nome">
             ${valorInput}
         </div>
 
@@ -42,6 +42,27 @@ function addTarefa() {
 function deletar(id) {
     var tarefa = document.getElementById(id);
     tarefa.remove();
+}
+
+// funcao responsavel por marcar a tarefa
+function marcarTarefa(id) {
+    var item = document.getElementById(id)
+    var classe = item.getAttribute('class')
+    console.log(classe);
+
+    if (classe == "item") {
+        item.classList.add('clicado');
+
+        var icone =  document.getElementById(`icone_`+id);
+        icone.classList.remove("iconoir-circle");
+        icone.classList.add("iconoir-check-circle");
+    }else{
+        item.classList.remove('clicado');
+
+        var icone =  document.getElementById(`icone_`+id);
+        icone.classList.add("iconoir-circle");
+        icone.classList.remove("iconoir-check-circle");
+    }
 }
 
 // fazendo que a tecla ENTER possa adicionar uma tarefa
